47480438a5c6155f1d84e33dc7da46fadf63c8b7
Jared <ree616916@gmail.com>
2026-09-21 13:53:54 -0400

    Testing projects.ts

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,4 +1,5 @@
 export const projects = [
   { name: "Project 1", url: "https://github.com/heatedlamp247/your-repo-1" },
   { name: "Project 2", url: "https://github.com/heatedlamp247/your-repo-2" }
+  { name: "Project 3", url: "https://github.com/heatedlamp247/your-repo-2" }
 ];
