b41de171e73e2209b43942863baea22462213f64
Jared <ree616916@gmail.com>
2026-09-21 22:43:03 -0400

    Added Project.ts

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,5 +1,5 @@
 export const projects = [
-  { name: "Project 1", url: "https://github.com/heatedlamp247/your-repo-1" },
+  { name: "Active Directory", url: "https://github.com/heatedlamp247/activedir" },
   { name: "Project 2", url: "https://github.com/heatedlamp247/your-repo-2" }
 ];
 // Make sure to add a coma for the 2nd last line. 
